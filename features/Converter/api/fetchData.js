89e935ff9420dc4bfe665eaa497f13f051043b8d
almustanir <olajidemubarak561@gmail.com>
2024-04-12 22:42:40 +0100

    5

--- features/Converter/api/fetchData.js
+++ features/Converter/api/fetchData.js
@@ -2,7 +2,14 @@ import { axios } from "../../../lib/axios";
 
 export const fetchRates = async (currencyOne) => {
     const {data} = await axios.get(
-        `/latest?base=${currencyOne}&apiKey${import.meta.env.VITE_API_KEY}`
-    )
+        `/latest?base=${currencyOne}&apikey=${import.meta.env.VITE_API_KEY}`
+    );
+    return data;
 };
-export const fetchSymbols = async () => {}
+
+export const fetchSymbols = async () => {
+    const {data} = await axios.get(
+        `/symbols?apikey=${import.meta.env.VITE_API_KEY}`
+    );
+    return data;
+}
--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -1,9 +1,25 @@
+import {useQueries} from "@tanstack/react-query"
 import { useState } from "react"
+import { fetchRates, fetchSymbols } from "../../Converter/api/fetchData";
 
 export const useCurrency = () => {
     const [amount, setAmount] = useState(25);
     const [currencyOne, setCurrencyOne] = useState("GBP");
     const [currencyTwo, setCurrencyTwo] = useState("EUR");
+
+    const [ratesData, symbolsData] = useQueries({
+        queries:[
+        {
+        queryKey:["rates", currencyOne],
+        queryFn: () => fetchRates(currencyOne),
+        staleTime: Infinity
+    },
+    {
+        queryKey: ['symbols'],
+        queryFn: () => fetchSymbols,
+        staleTime: Infinity
+    }
+]});
 
     return { amount, currencyOne, currencyTwo};
 };
