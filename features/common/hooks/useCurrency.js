d146a7cf13eb3d361190960b31042f629e0fb40f
almustanir <olajidemubarak561@gmail.com>
2024-04-13 20:12:10 +0100

    7

--- features/common/hooks/useCurrency.js
+++ features/common/hooks/useCurrency.js
@@ -1,32 +1,56 @@
-import {useQueries} from "@tanstack/react-query"
-import { useState } from "react"
+import { useQueries } from "@tanstack/react-query";
+import { useState } from "react";
 import { fetchRates, fetchSymbols } from "../../Converter/api/fetchData";
 
 export const useCurrency = () => {
-    const [amount, setAmount] = useState(25);
-    const [currencyOne, setCurrencyOne] = useState("GBP");
-    const [currencyTwo, setCurrencyTwo] = useState("EUR");
+  const [amount, setAmount] = useState(25);
+  const [currencyOne, setCurrencyOne] = useState("GBP");
+  const [currencyTwo, setCurrencyTwo] = useState("EUR");
 
-    const [ratesData, symbolsData] = useQueries({
-        queries: [
-        {
-        queryKey:["rates", currencyOne],
+  const [ratesData, symbolsData] = useQueries({
+    queries: [
+      {
+        queryKey: ["rates", currencyOne],
         queryFn: () => fetchRates(currencyOne),
         staleTime: Infinity,
-        select: ({rates,date,timestamp}) => {
-            return {rates,date,timestamp};
-        }
-    },
-    {
-        queryKey: ['symbols'],
+        select: ({ rates, date, timestamp }) => {
+          return { rates, date, timestamp };
+        },
+      },
+      {
+        queryKey: ["symbols"],
         queryFn: () => fetchSymbols,
         staleTime: Infinity,
-        select: ({symbols}) => symbols
-    }
-]
-});
+        select: ({ symbols }) => symbols,
+      },
+    ],
+  });
 
-    console.log(ratesData);
+  const isLoading = [ratesData, symbolsData].some((query) => query.isLoading);
+  const isError = [ratesData, symbolsData].some((query) => query.isError);
 
-    return { amount, currencyOne, currencyTwo};
+  const convertedAmount = (ratesData.data?.rates[currencyTwo] * amount).toFixed(
+    2
+  );
+  const date = new Date(ratesData.data?.date).toLocaleDateString();
+  const time = new Date(ratesData.data?.time).toLocaleTimeString("en-US");
+
+  const currencyList = symbolsData.data ? Object.keys(symbolsData.data) : [];
+
+  return {
+    amount,
+    setAmount,
+    currencyOne,
+    currencyTwo,
+    setCurrencyOne,
+    setCurrencyTwo,
+    currencyList,
+    convertedAmount,
+    ratesData,
+    symbolsData,
+    date,
+    time,
+    isLoading,
+    isError
+  };
 };
